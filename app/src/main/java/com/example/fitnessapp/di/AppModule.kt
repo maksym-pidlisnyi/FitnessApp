ec5f2bdf11f31349c82deef36854475b92566c12
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-01-31 20:25:10 +0200

    add di using dagger-hilt

---
+++ app/src/main/java/com/example/fitnessapp/di/AppModule.kt
@@ -0,0 +1,59 @@
+package com.example.fitnessapp.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import androidx.room.Room
+import com.example.fitnessapp.db.RunDao
+import com.example.fitnessapp.db.RunningDatabase
+import com.example.fitnessapp.util.Constants.Companion.DATABASE_NAME
+import com.example.fitnessapp.util.Constants.Companion.KEY_FIRST_TIME_TOGGLE
+import com.example.fitnessapp.util.Constants.Companion.KEY_NAME
+import com.example.fitnessapp.util.Constants.Companion.KEY_WEIGHT
+import com.example.fitnessapp.util.Constants.Companion.SHARED_PREFERENCES_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDb(app: Application): RunningDatabase {
+        return Room.databaseBuilder(app, RunningDatabase::class.java, DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase): RunDao {
+        return db.getRunDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(app: Application) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPreferences: SharedPreferences) = sharedPreferences.getBoolean(
+        KEY_FIRST_TIME_TOGGLE, true
+    )
+
+}
---
+++ app/src/main/java/com/example/fitnessapp/util/Constants.kt
@@ -0,0 +1,51 @@
+package com.example.fitnessapp.util
+
+import android.graphics.Color
+import com.github.mikephil.charting.data.LineDataSet
+
+class Constants {
+
+    companion object {
+        const val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+        const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
+        // Database
+        const val DATABASE_NAME = "running_db"
+
+        // Tracking Options
+        const val LOCATION_UPDATE_INTERVAL = 5000L
+        const val FASTEST_LOCATION_UPDATE_INTERVAL = 2000L
+
+        // Map Options
+        const val POLYLINE_COLOR = Color.RED
+        const val POLYLINE_WIDTH = 8f
+        const val MAP_ZOOM = 15f
+
+        // Timer
+        const val TIMER_UPDATE_INTERVAL = 50L
+
+        // LineChart
+        val LINE_DATA_MODE = LineDataSet.Mode.CUBIC_BEZIER
+
+        // MapView
+        const val MAP_VIEW_HEIGHT_IN_DP = 200f
+
+        // Notifications
+        const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+        const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+        const val NOTIFICATION_ID = 1
+
+        // Shared Preferences
+        const val SHARED_PREFERENCES_NAME = "sharedPref"
+        const val KEY_NAME = "KEY_NAME"
+        const val KEY_WEIGHT = "KEY_WEIGHT"
+        const val KEY_FIRST_TIME_TOGGLE = "KEY_FIRST_TIME_TOGGLE"
+
+        // Service Intent Actions
+        const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+        const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_SERVICE"
+        const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+        const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+    }
+}
