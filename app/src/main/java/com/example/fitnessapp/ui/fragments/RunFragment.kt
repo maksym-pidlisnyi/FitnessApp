0e54d8b48709bd8255fe5536a9be9ebef73667d7
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-02-07 15:33:39 +0200

    add easy permissions with its requests

--- app/src/main/java/com/example/fitnessapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/fitnessapp/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.example.fitnessapp.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +11,15 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.fitnessapp.R
 import com.example.fitnessapp.databinding.FragmentRunBinding
+import com.example.fitnessapp.util.Constants
+import com.example.fitnessapp.util.TrackingUtility
 import com.example.fitnessapp.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks{
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -30,9 +36,52 @@ class RunFragment : Fragment(R.layout.fragment_run) {
     // For testing, will remove soon
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        requestPermission()
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
     }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    private fun requestPermission() {
+        if (TrackingUtility.hasLocationPermissions(requireContext()))
+            return
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You should accept these permissions to access app",
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You should accept these permissions to access app",
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/fitnessapp/util/TrackingUtility.kt
@@ -0,0 +1,55 @@
+package com.example.fitnessapp.util
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+import java.util.concurrent.TimeUnit
+
+class TrackingUtility {
+
+    companion object {
+        /**
+         * Checks if the user accepted the necessary location permissions or not
+         */
+        fun hasLocationPermissions(context: Context) =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                EasyPermissions.hasPermissions(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } else {
+                EasyPermissions.hasPermissions(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            }
+        }
+
+        /**
+         * Takes an amount of milliseconds and converts it to a formatted string, optionally
+         * with milliseconds
+         */
+        fun getFormattedStopWatchTime(ms: Long, includeMillis: Boolean = false): String {
+            var milliseconds = ms
+            val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+            milliseconds -= TimeUnit.HOURS.toMillis(hours)
+            val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+            milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+            val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+            if (!includeMillis) {
+                return "${if (hours < 10) "0" else ""}$hours:" +
+                        "${if (minutes < 10) "0" else ""}$minutes:" +
+                        "${if (seconds < 10) "0" else ""}$seconds"
+            }
+            milliseconds -= TimeUnit.SECONDS.toMillis(seconds)
+            milliseconds /= 10
+            return "${if (hours < 10) "0" else ""}$hours:" +
+                    "${if (minutes < 10) "0" else ""}$minutes:" +
+                    "${if (seconds < 10) "0" else ""}$seconds:" +
+                    "${if (milliseconds < 10) "0" else ""}$milliseconds"
+        }
+}
